212a835dc877b335e541f86372db93260699b8bf
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-10-17 00:06:02 +0000

    d

--- PP2_3.py
+++ PP2_3.py
@@ -1,12 +1,46 @@
-
+'''
+    Lesson: Else if
+    Author: Dinara Pathiraja
+    Date Created: Oct 16, 2024
+    Date Last Modified: Oct 16, 2024
+'''
 
 def q1(): 
-  #Write Assignment code here
-
-
+  word= input("In: ")
+  if (word[-1:]=="-y"):
+    print("ies")
+  elif(word[-2:]=="-ey"): 
+    print("eys")
+  elif(word[-1:]=="-ife"):
+    print("-ives")
+  else:
+    print("-s")
+  
 def q2(): 
-  #Write Assignment code here
-
+  num= input ("Input an integer: ")
+  num = int(num)
+  if(num>0):
+    print(f"{num} is positive")
+  elif (num<0):
+    print(f"{num} is negative")
+  else: 
+    print()
+def q3():
+  side1 = input("Input a number: ")
+  side1= float(side1)
+  side2 = input("Input a number: ")
+  side2= float(side2)
+  side3 = input("Input a number: ")
+  side3= float(side3)
+  #if ((side1+side2>side3) or(side2+side3>side1) or (side1+side3>side2)):
+  if(side1==side2==side3):
+    print("Equilateral")
+  elif((side1 == side2 != side3) or (side1 == side3 != side2)or (side2 == side3 != side1)):
+    print ("Isosceles")
+  elif((side1!=side2!=side3)):
+    print("Scalene")
+  else:
+    print("No Triangle")
 
 
 #Do not alter the following code
@@ -14,3 +48,4 @@ def q2():
 
 q1()
 q2()
+q3()
