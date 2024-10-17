17887bac112d6d41dee11c40ae9509ecbc2e4937
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-10-17 02:06:59 +0000

    d

--- PP2_3.py
+++ PP2_3.py
@@ -7,23 +7,23 @@
 
 def q1(): 
   word= input("In: ")
-  if (word[-1:]=="-y"):
-    print("ies")
-  elif(word[-2:]=="-ey"): 
-    print("eys")
-  elif(word[-1:]=="-ife"):
+  if(word[-3:]=="ife"):
     print("-ives")
+  elif(word[-2:]=="ey"):
+    print("-eys")
+  elif (word[-1:]=="y"):
+    print("-ies")
   else:
     print("-s")
   
 def q2(): 
-  num= input ("Input an integer: ")
-  num = int(num)
-  if(num>0):
-    print(f"{num} is positive")
-  elif (num<0):
-    print(f"{num} is negative")
-  else: 
+  num= input ("In:")
+  num1 = int(num)
+  if(num1>0):
+    print(f"{num1} is positive")
+  elif (num1<0):
+    print(f"{num1} is negative")
+  elif(num1==0):
     print()
 def q3():
   side1 = input("Input a number: ")
@@ -32,19 +32,20 @@ def q3():
   side2= float(side2)
   side3 = input("Input a number: ")
   side3= float(side3)
-  if(side1==side2==side3):
-    print("Equilateral")
-  elif((side1 == side2 != side3) or (side1 == side3 != side2)or (side2 == side3 != side1)):
-    print ("Isosceles")
-  elif((side1!=side2!=side3)):
-    print("Scalene")
-  else:
+  if(side1+side2>side3 or side2+side3>side1 or side1+side3>side2 ):
+    if(side1==side2 and side1==side3):
+     print("Equilateral")
+    elif((side1 == side2) or (side1 == side3)or (side2 == side3)):
+     print ("Isosceles")
+    elif(side1!=side2 and side1!=side3):
+     print("Scalene")
+  else: 
     print("No Triangle")
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
+# q1()
+# q2()
+# q3()
